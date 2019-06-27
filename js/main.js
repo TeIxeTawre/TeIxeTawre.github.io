bb906d1b1a978579a7a411a9bec5c2b44f26e82f
TeIxe <teixetawrex@gmail.com>
2019-06-27 16:23:05 +0300

    add project

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$( document ).ready( function () {
+	
+	$('.menu-btn').on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('active-btn');
+		$('.menu').toggleClass('active');
+	});
+
+	$('.bg-menu').on('click', function() {
+		$('.menu-btn').toggleClass('active-btn');
+		$('.menu').toggleClass('active');
+	});
+	
+	$('.arrow-left').on('click', function (e) {
+		e.preventDefault();
+		$('.owl-carousel').trigger('prev.owl.carousel');
+	});
+
+	$('.arrow-right').on('click', function (e) {
+		e.preventDefault();
+		$('.owl-carousel').trigger('next.owl.carousel');
+	});
+
+	$('.owl-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: false,
+		autoplaySpeed: 1000,
+		smartSpeed: 1000
+	});
+
+});
