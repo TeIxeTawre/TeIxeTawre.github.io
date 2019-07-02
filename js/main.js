cf3894bd0751868ae40754a02d58fe3c2bdcb0ea
TeIxe <teixetawrex@gmail.com>
2019-07-02 14:47:04 +0300

    add many functions for header and add second slider

--- js/main.js
+++ js/main.js
@@ -13,15 +13,15 @@ $( document ).ready( function () {
 	
 	$('.arrow-left').on('click', function (e) {
 		e.preventDefault();
-		$('.owl-carousel').trigger('prev.owl.carousel');
+		$('#header-carousel').trigger('prev.owl.carousel');
 	});
 
 	$('.arrow-right').on('click', function (e) {
 		e.preventDefault();
-		$('.owl-carousel').trigger('next.owl.carousel');
+		$('#header-carousel').trigger('next.owl.carousel');
 	});
 
-	$('.owl-carousel').owlCarousel({
+	$('#header-carousel').owlCarousel({
 		items: 1,
 		loop: true,
 		autoplay: true,
@@ -29,6 +29,57 @@ $( document ).ready( function () {
 		autoplayHoverPause: false,
 		autoplaySpeed: 1000,
 		smartSpeed: 1000
+	});
+
+	$('#simple-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		autoplaySpeed: 1000,
+		mouseDrag: false,
+		onDragged: callback
+	});
+
+	function callback(e) {
+
+		$('#simple-carousel').on('changed.owl.carousel', function (e) {
+			let curItem = 0;
+			curItem = e.item.index;
+
+			if ( curItem == 0) {
+				$('.sm-one').addClass('active-trg');
+				$('.sm-two').removeClass('active-trg');
+				$('.sm-thre').removeClass('active-trg');
+			}
+			if ( curItem == 1) {
+				$('.sm-two').addClass('active-trg');
+				$('.sm-one').removeClass('active-trg');
+				$('.sm-thre').removeClass('active-trg');
+			}
+			if ( curItem == 2) {
+				$('.sm-three').addClass('active-trg');
+				$('.sm-two').removeClass('active-trg');
+				$('.sm-one').removeClass('active-trg');
+			}
+		});
+	}
+
+
+	$('.sm-one').on('click', function (e) {
+		e.preventDefault();
+		$('#simple-carousel').trigger("to.owl.carousel", [0, 500]);
+	});
+
+	$('.sm-two').on('click', function (e) {
+		e.preventDefault();
+		$('#simple-carousel').trigger('to.owl.carousel', [1, 500]);		
+	});
+
+	$('.sm-three').on('click', function (e) {
+		e.preventDefault();
+		$('#simple-carousel').trigger('to.owl.carousel', [2, 500]);
 	});
 
 });
